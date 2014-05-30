2229eae7dbabe2c98d7a7c6e0dedd81df30c5095
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-30 11:01:52 +0100

    switched from nature to command-line-args

--- bin/cli.js
+++ bin/cli.js
@@ -4,13 +4,13 @@ var mfs = require("more-fs"),
     path = require("path"),
     loadConfig = require("config-master"),
     boil = require("boil-js"),
-    Model = require("nature").Model;
+    parseArgv = require("command-line-args");
 
-var argv = new Model()
-    .define({ name: "files", type: Array, defaultOption: true })
-    .define({ name: "prefix", type: "string" })
-    .define({ name: "changefreq", type: "string", value: "daily" })
-    .set(process.argv);
+var argv = parseArgv([
+    { name: "files", type: Array, defaultOption: true },
+    { name: "prefix", type: "string" },
+    { name: "changefreq", type: "string", value: "daily" }
+]);
     
 var boilHbs = mfs.read(path.join(__dirname, "..", "boil.hbs"));
 boilHbs = boil.render(boilHbs, { 
