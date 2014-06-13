93da5c3a29c430baeb1f71fca9e42f79bd79cb3f
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-13 10:31:03 +0100

    deps

--- bin/cli.js
+++ bin/cli.js
@@ -4,13 +4,13 @@ var mfs = require("more-fs"),
     path = require("path"),
     loadConfig = require("config-master"),
     boil = require("boil-js"),
-    parseArgv = require("command-line-args");
+    cliArgs = require("command-line-args");
 
-var argv = parseArgv([
+var argv = cliArgs([
     { name: "files", type: Array, defaultOption: true },
-    { name: "prefix", type: "string" },
-    { name: "changefreq", type: "string", value: "daily" }
-]);
+    { name: "prefix", type: String },
+    { name: "changefreq", type: String, value: "daily" }
+]).parse();
     
 var boilHbs = mfs.read(path.join(__dirname, "..", "boil.hbs"));
 boilHbs = boil.render(boilHbs, { 
